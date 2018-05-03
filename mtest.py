6a5de95360d1f73208b3171c19072de3c6cacb1f
Jory Schossau <joryschossau@gmail.com>
2018-05-03 17:32:23 -0400

    fixed branch, commit hash, and dir removal to work as intended

--- mtest.py
+++ mtest.py
@@ -16,7 +16,8 @@ from utils.helpers import test_repo_name, product, dir_base, dir_test, path_base
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c','--compare', default = 'master', help='branch or commit hash to compare.')
+    parser.add_argument('branch', nargs='?', default='master')
+    parser.add_argument('commit', nargs='?', default='HEAD')
     parser.add_argument('-f','--force', action='store_true', required = False, default = False, help='force new download and compile')
     args = parser.parse_args()
     compile_default_projects(args)
@@ -24,9 +25,11 @@ def main():
 
 def compile_default_projects(args):
     if not os.path.isfile(os.path.join(dir_base,product)) or args.force:
-        print("clone new",args.compare,"as baseline", flush=True)
-        shutil.rmtree(dir_base)
-        repo = git.Repo.clone_from("https://github.com/hintzelab/mabe", dir_base, branch=args.compare)
+        print("clone new",args.branch,"at",args.commit,"as baseline", flush=True)
+        shutil.rmtree(dir_base,ignore_errors=True)
+        repo = git.Repo.clone_from("https://github.com/hintzelab/mabe", dir_base, branch=args.branch)
+        revision = repo.create_head('revision',args.commit)
+        repo.heads.revision.checkout()
         os.chdir(dir_base)
         print("building", flush=True)
         subprocess.run("python pythonTools/mbuild.py -p"+str(psutil.cpu_count(logical=False)), shell=True, check=True)
