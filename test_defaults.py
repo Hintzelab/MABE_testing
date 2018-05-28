eaee44d21b9aff13227afdb0cd381e60d2937421
Jory Schossau <joryschossau@gmail.com>
2018-05-27 21:05:12 -0400

    diff -> ABdiff because I remembered why it was named that

--- test_defaults.py
+++ test_defaults.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 from utils.helpers import this_repo_path, product, basename_base, basename_test, path_base_exe, path_test_exe
-from utils.helpers import cd, diff, runCmdAndHideOutput, runCmdAndShowOutput
+from utils.helpers import cd, ABdiff, runCmdAndHideOutput, runCmdAndShowOutput
 
 ## all tests run in order and are run if they begin with 'test_'
 def test_startup():
@@ -17,19 +17,19 @@ def test_startup():
     runCmdAndHideOutput( "{exe} -p GLOBAL-outputDirectory {path}".format(exe=path_test_exe, path=basename_test) )
 
 def test_max_csv():
-    diff('max.csv')
+    ABdiff('max.csv')
 def test_pop_csv():
-    diff('pop.csv')
+    ABdiff('pop.csv')
 def test_lod_data_csv():
-    diff('LOD_data.csv')
+    ABdiff('LOD_data.csv')
 def test_lod_organisms_csv():
-    diff('LOD_organisms.csv')
+    ABdiff('LOD_organisms.csv')
 def test_settings_cfg():
-    diff('settings.cfg')
+    ABdiff('settings.cfg')
 def test_settings_organism_cfg():
-    diff('settings_organism.cfg')
+    ABdiff('settings_organism.cfg')
 def test_settings_world_cfg():
-    diff('settings_world.cfg')
+    ABdiff('settings_world.cfg')
 
 def test_shutdown():
     pass
